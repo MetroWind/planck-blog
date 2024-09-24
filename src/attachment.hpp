f6f631684d99d75acf9be2cfdf4c6371ebe2f719
MetroWind <chris.corsair@gmail.com>
2024-09-24 13:37:44 -0700

    Implement class for SHA256 hashing.

--- src/attachment.hpp
+++ src/attachment.hpp
@@ -2,7 +2,9 @@
 
 #include <string>
 
+#include "config.hpp"
 #include "utils.hpp"
+#include "hash.hpp"
 
 struct Attachment
 {
@@ -10,4 +12,20 @@ struct Attachment
     std::string hash;
     Time upload_time;
     std::string content_type;
+};
+
+class AttachmentManager
+{
+public:
+    explicit AttachmentManager(HasherInterface& h): hasher(h) {}
+
+    // Get an attachment object out of some bytes.
+    Attachment attachmentFromBytes(const std::string& bytes,
+                                   std::string_view filename) const;
+    // Get the path in the local file system of the attachment,
+    // relative to the attachment dir set in the config.
+    std::string path(const Attachment& att) const;
+
+private:
+    HasherInterface& hasher;
 };
